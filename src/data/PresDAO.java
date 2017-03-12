218e196b1d52e5a56156d88746a0d7de63d6246a
Greg Taylor <hesitatetowonder@gmail.com>
2017-03-12 15:02:45 -0600

    refactored

--- src/data/PresDAO.java
+++ src/data/PresDAO.java
@@ -5,7 +5,7 @@ import java.util.*;
 public interface PresDAO {
 	
 	public List<President> loadPresidentsFromFile();
-	public List<President> getPresidentFactFromFile();
+//	public List<President> getPresidentFactFromFile();
 	public Map<Integer, President> getHashMapFromArrayList();
 	public President getPresidentByTerm(Integer term);
 	public List<President>getPicturePath();
